370f34ec1c20ade180e6edc9ede879ac197fa58b
Daniel Santillan <daniel.santillan@eox.at>
2014-11-10 10:42:03 +0100

    Tests for performance

--- vires/processes/retrieve_czml.py
+++ vires/processes/retrieve_czml.py
@@ -198,7 +198,7 @@ class retrieve_czml(Component):
         for lon, lat, r, f in izip (lons, lats, rads, fs):
             clr = cs.to_rgba(f)
             id = str(uuid4())
-            tmp.write(',{"id":"%s","point":{"pixelSize":10,"show":true,"color":{"rgba":[%d,%d,%d]}},'
+            tmp.write(',{"id":"%s","point":{"pixelSize":10,"show":true,"color":{"rgba":[%d,%d,%d,255]}},'
                 %(id,int(clr[0]*256),int(clr[1]*256),int(clr[2]*256)))
             tmp.write('"position":{"cartographicDegrees":[%f,%f,%d]}}'%(lon, lat, int(r-6384000)))
 
